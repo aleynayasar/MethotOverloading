ee102e0aa216ed72e5fa315951fa3f74cd747e55
aleynayasar <79570388+aleynayasar@users.noreply.github.com>
2022-03-02 15:13:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MethotOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, answer;
+            Console.Write("Please, enter your name : ");
+            name = Console.ReadLine();
+            Console.Write("Please, enter your surname : ");
+            surname = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(surname))
+            {
+                answer = Message(name);
+            }
+            else
+                answer = Message(name, surname);
+
+            Console.WriteLine(answer);
+            Console.ReadKey();
+        }
+
+        static string Message(string name)
+        {
+            string firstLetter = name.Substring(0, 1).ToUpper();
+            string theRest = name.Substring(1).ToLower();
+            string text = "Dear" + firstLetter + theRest + ", your welcome.";
+            return text;
+
+        }
+
+        static string Message(string name , string surname)
+        {
+            string firstLetter = name.Substring(0, 1).ToUpper();
+            string theRest = name.Substring(1).ToLower();
+
+            string firstLetter1 = surname.Substring(0, 1).ToUpper();
+            string theRest1 = surname.Substring(1).ToLower();
+            string text = "Dear "+ firstLetter + theRest +firstLetter1+theRest1+", your welcome.";
+            return text;
+        }
+    }
+}
